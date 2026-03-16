5cf8d6e13fbf24c5afdfb982fc237febc29d6124
Botmasterkenya <titotosh613@gmail.com>
2026-03-16 15:55:05 +0300

    First commit

---
+++ src/TemperatureReadings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TemperatureReadings{
+
+    public static void main(String[] args){
+
+        Scanner keyboard = new Scanner(System.in);
+        double[] temperature;
+        temperature =new double[7];
+        for (int i = 0; i < temperature.length; i++){
+            System.out.println("enter max temperature for the day " + (i +1));
+        }
+
+    }
+}
+
+
+
