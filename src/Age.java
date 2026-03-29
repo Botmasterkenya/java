71d1b87282bc64f47516b22c2f5598aca9af9ce2
Botmasterkenya <titotosh613@gmail.com>
2026-03-29 18:21:49 +0300

    My java commit today

---
+++ src/Age.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if (age <= 18){
+            System.out.println("Chill you are a minor");
+        }
+        else{
+            System.out.println("Adult");
+        }
+
+
+
+    }
+}
---
+++ src/Iteration.java
@@ -0,0 +1,16 @@
+
+public class Iteration {
+    public static void main(String [] args){
+
+        int[] numbers = {10, 20, 30};
+        System.out.println(numbers[0]); // prints 10
+        System.out.println(numbers[2]); // prints 30
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+
+
+    }
+}
